9deed159d315cbbaf0418a046fff1e1138da0ff0
evgenii_mizulin <evgeny.mizulin@gmail.com>
2019-05-27 01:15:59 +0300

    isDemo flag

--- src/landing/components/Landing.js
+++ src/landing/components/Landing.js
@@ -4,6 +4,7 @@ import { Button } from 'src/lib/components/Button'
 import YouTube from 'react-youtube'
 import { Link } from 'react-router-dom'
 import { routes } from 'src/lib/services/Routes'
+import { config } from 'src/config'
 
 import { cn } from './Landing.style'
 
@@ -26,20 +27,28 @@ export class Landing extends React.Component {
           </Link>
         </div>
         <div className={cn.body}>
-          <div className={`${cn.message} ${cn.firstMessage}`}>
-            Don't want to register? Use test account: login - test, password - testtest.
-          </div>
-          <div className={`${cn.message} mt-md`}>
-            <div className="pb-sm">We need your support.</div>
-            <div className="pb-sm">
-              Your even small donation will help us make nice product for you.{' '}
-              <a target="_blank" rel="noopener noreferrer" href="https://money.yandex.ru/to/410011439375102">
-                Donate!
-              </a>
+          <div className={cn.messages}>
+            {!config.isDemo ? null : (
+              <div className={cn.message}>
+                Don't want to register? Use test account: login - test, password - testtest.
+              </div>
+            )}
+            <div className={`${cn.message}`}>
+              <div className="pb-sm">We need your support.</div>
+              <div className="pb-sm">
+                Your even small donation will help us make nice product for you.{' '}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://money.yandex.ru/to/410011439375102"
+                >
+                  Donate!
+                </a>
+              </div>
             </div>
           </div>
           <div className={`${cn.title} pb-sm`}>
-            Headless{' '}
+            Open source headless{' '}
             <acronym title="Content Management System" lang="en">
               CMS
             </acronym>{' '}
@@ -49,23 +58,25 @@ export class Landing extends React.Component {
             </acronym>{' '}
             to&nbsp;any app
           </div>
-          <div className={`${cn.explanation} pb-lg mb-lg`}>
-            It's the demo server, don't use in production environment
+          {!config.isDemo ? null : (
+            <div className={cn.explanation}>It's the demo server, don't use in production environment</div>
+          )}
+          <div className={`pt-lg mt-lg`}>
+            <YouTube
+              videoId="XLUM4URSWjw"
+              opts={{
+                width: 760,
+                height: 432,
+                playerVars: {
+                  // controls: 0,
+                  iv_load_policy: 3,
+                  modestbranding: 1,
+                  rel: 0,
+                  showinfo: 0,
+                },
+              }}
+            />
           </div>
-          <YouTube
-            videoId="XLUM4URSWjw"
-            opts={{
-              width: 760,
-              height: 432,
-              playerVars: {
-                // controls: 0,
-                iv_load_policy: 3,
-                modestbranding: 1,
-                rel: 0,
-                showinfo: 0,
-              },
-            }}
-          />
           <div className="pt-lg mt-lg text-center pb-lg">
             Created by <a href="mailto:evgeny.mizulin@gmail.com">Evgeny Mizulin</a>
           </div>
